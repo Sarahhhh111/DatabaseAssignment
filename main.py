54f144148ffaf7ddc53be0740d7314f5f38c7283
Sarahhhh111 <142394867+Sarahhhh111@users.noreply.github.com>
2025-04-07 20:55:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -65,8 +65,3 @@ async def get_scores():
         scores.append(doc)
     return scores
 
-app = app
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
